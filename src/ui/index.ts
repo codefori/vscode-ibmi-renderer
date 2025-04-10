08199aea81c96c121e93c0b8d5ae97c750497e22
worksofliam <mrliamallan@live.co.uk>
2025-04-10 19:36:17 -0400

    Init commit
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

---
+++ src/ui/index.ts
@@ -0,0 +1,76 @@
+import { readFile, readFileSync } from "fs";
+import { WebviewViewProvider, WebviewView, Uri, CancellationToken, WebviewViewResolveContext, Webview, DiagnosticSeverity, window, WebviewPanel, ViewColumn, ExtensionContext } from "vscode";
+import { basename } from "path";
+
+
+export class RendererWebview {
+  private view: WebviewPanel;
+
+  private get extensionPath() {
+    return this.context.extensionUri;
+  }
+
+  constructor(private readonly context: ExtensionContext, private readonly workingUri: Uri) {
+    const panel = window.createWebviewPanel(`ibmi_renderer`, `Renderer`, {
+      preserveFocus: true,
+      viewColumn: ViewColumn.Active
+    });
+
+    panel.webview.options = {
+      enableScripts: true,
+      enableCommandUris: true,
+      localResourceRoots: [
+        this.extensionPath,
+        Uri.joinPath(this.extensionPath, 'webui'),
+        Uri.joinPath(this.extensionPath, 'webui', `scripts`),
+      ],
+    };
+
+    panel.webview.onDidReceiveMessage(this.onDidGetMessage.bind(this));
+
+    panel.webview.html = this.getBaseHtml(panel.webview);
+
+    this.view = panel;
+  }
+
+  show() {
+    if (this.view) {
+      this.view.reveal();
+    }
+  }
+
+  private onDidGetMessage(message: any) {
+  }
+
+  private getBaseHtml(webview: Webview) {
+    const basePath = toUri(webview, this.extensionPath, `webui`, `index.html`);
+    // async might be better
+    let content = readFileSync(basePath.fsPath, "utf-8");
+
+    const fileVariables = {
+      '{elements}': toUri(webview, this.extensionPath, `webui`, `scripts`, `vscode-elements.js`),
+      '{styles}': toUri(webview, this.extensionPath, `webui`, `styles.css`),
+      '{codicon}': toUri(webview, this.extensionPath, `webui`, `scripts`, `codicon.css`),
+    };
+
+    // Replace all variables in the content
+    for (const [key, value] of Object.entries(fileVariables)) {
+      const regex = new RegExp(key, 'g');
+      content = content.replace(regex, value.toString());
+    }
+
+    return content;
+  }
+
+  static getCommandHref(command: string, ...args: unknown[]) {
+    return `command:${command}?${encodeURIComponent(JSON.stringify(args))}`;
+  }  
+}
+
+function toUri(
+  webview: Webview,
+  extensionUri: Uri,
+  ...pathList: string[]
+) {
+  return webview.asWebviewUri(Uri.joinPath(extensionUri, ...pathList));
+}
